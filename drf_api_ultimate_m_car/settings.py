9054f7c654f19899e7458d75d7242cea2f3c5045
JoeYip13 <>
2023-10-28 14:00:17 +0000

    feat(profiles): add 'profiles' app with model structure

--- drf_api_ultimate_m_car/settings.py
+++ drf_api_ultimate_m_car/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-joeyip13-project-5-drf-a-qqkxq0z58x.us2.codeanyapp.com']
 
 
 # Application definition
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
