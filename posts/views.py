5692e8a117aa19b6b33d3414d97628a22762cb71
JoeYip13 <>
2023-10-28 20:01:45 +0000

    feat(views): intergrate django-filter into 'posts' views.py

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api_ultimate_m_car.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -19,6 +20,15 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        # User feed
+        'owner__followed__owner__profile',
+        # User liked posts
+        'likes__owner__profile',
+        # user posts
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
