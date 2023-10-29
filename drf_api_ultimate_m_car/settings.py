a245cc7c19473424767454360e8842272d4376e8
JoeYip13 <>
2023-10-29 13:34:31 +0000

    chore(settings): remove staticfiles storage

--- drf_api_ultimate_m_car/settings.py
+++ drf_api_ultimate_m_car/settings.py
@@ -200,7 +200,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
