d04be9b21a5303bb892d1da3beca3fc321ff7ae2
JoeYip13 <>
2023-10-30 13:54:11 +0000

    feat(routing): update nav links to use react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
+// CSS Styles
 import styles from './App.module.css';
+
+// Bootstrap Components
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/esm/Container';
+
+// React router
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home page</h1>} />
+                    <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+                    <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+                    <Route render={() => <p>Page not found!</p>} />
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,17 +11,22 @@ import logo from '../assets/M-logo.png';
 // CSS Styles
 import styles from '../styles/NavBar.module.css';
 
+// React router
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand className={styles.logo}>Ultimate <img src={logo} alt="logo" height="45" /> Car</Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand className={styles.logo}>Ultimate <img src={logo} alt="logo" height="45" /> Car</Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link><i class="fa-solid fa-house"></i>Home</Nav.Link>
-                        <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
-                        <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/"><i class="fa-solid fa-house"></i>Home</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/signup"><i class="fa-solid fa-user-plus"></i>Sign Up</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/signin"><i class="fa-solid fa-right-to-bracket"></i>Sign In</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
