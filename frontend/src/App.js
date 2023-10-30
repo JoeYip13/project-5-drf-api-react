0b2d462db4d030be84713af418d1f2b1ed7c1f52
JoeYip13 <>
2023-10-30 19:48:24 +0000

    feat(api): send request to verify current logged in user
    Also create context to pass the data fetched from the API through the application.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,59 @@
+// React
+import { createContext, useEffect, useState } from "react";
+
 // CSS Styles
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 // Bootstrap Components
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/esm/Container';
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/esm/Container";
 
 // React router
 import { Route, Switch } from "react-router-dom";
 
 // Axios
-import './api/axiosDefaults.js';
+import "./api/axiosDefaults.js";
+import axios from "axios";
 
 // Pages
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const handleMount = async () => {
+        try {
+            const { data } = await axios.get("dj-rest-auth/user/");
+            setCurrentUser(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        handleMount();
+    },[]);
+
     return (
-        <div className={styles.App}>
-            <NavBar />
-            <Container className={styles.Main}>
-                <Switch>
-                    <Route exact path="/" render={() => <h1>Home page</h1>} />
-                    <Route exact path="/signin" render={() => <SignInForm />} />
-                    <Route exact path="/signup" render={() => <SignUpForm />} />
-                    <Route render={() => <p>Page not found!</p>} />
-                </Switch>
-            </Container>
-        </div>
+        <CurrentUserContext.Provider value={currentUser}>
+            <SetCurrentUserContext.Provider value={setCurrentUser}>
+                <div className={styles.App}>
+                    <NavBar />
+                    <Container className={styles.Main}>
+                        <Switch>
+                            <Route exact path="/" render={() => <h1>Home page</h1>} />
+                            <Route exact path="/signin" render={() => <SignInForm />} />
+                            <Route exact path="/signup" render={() => <SignUpForm />} />
+                            <Route render={() => <p>Page not found!</p>} />
+                        </Switch>
+                    </Container>
+                </div>
+            </SetCurrentUserContext.Provider>
+        </CurrentUserContext.Provider>
     );
 }
 
