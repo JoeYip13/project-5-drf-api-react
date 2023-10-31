77d0dc5ce5b36bfc6737e6864800944018dc41d5
JoeYip13 <>
2023-10-31 19:14:03 +0000

    feat(like): add handle like and unlike a post

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Post = (props) => {
     const {
@@ -25,10 +26,51 @@ const Post = (props) => {
         image,
         updated_at,
         postPage,
+        setPosts,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+
+    const handleLike = async () => {
+        try {
+            const { data } = await axiosRes.post("/likes/", { post: id });
+            setPosts((prevPosts) => ({
+                ...prevPosts,
+                results: prevPosts.results.map((post) => {
+                    return post.id === id
+                        ? {
+                              ...post,
+                              likes_count: post.likes_count + 1,
+                              like_id: data.id,
+                          }
+                        : post;
+                }),
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleUnlike = async () => {
+        try {
+            await axiosRes.delete(`/likes/${like_id}/`);
+            setPosts((prevPosts) => ({
+                ...prevPosts,
+                results: prevPosts.results.map((post) => {
+                    return post.id === id
+                        ? {
+                              ...post,
+                              likes_count: post.likes_count - 1,
+                              like_id: null,
+                          }
+                        : post;
+                }),
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <Card className={styles.Post}>
@@ -69,13 +111,11 @@ const Post = (props) => {
                             <i class="fa-regular fa-heart"></i>
                         </OverlayTrigger>
                     ) : like_id ? (
-                        <span onClick={() => {}}>
-                            <i
-                                className={`fa-regular fa-heart`}
-                            />
+                        <span onClick={handleUnlike}>
+                            <i className={`fa-regular fa-heart`} />
                         </span>
                     ) : currentUser ? (
-                        <span onClick={() => {}}>
+                        <span onClick={handleLike}>
                             <i
                                 className={`fa-regular fa-heart ${styles.HeartOutline}`}
                             ></i>
@@ -85,9 +125,7 @@ const Post = (props) => {
                             placement="top"
                             overlay={<Tooltip>Log in to like posts</Tooltip>}
                         >
-                            <i
-                                className={`fa-regular fa-heart`}
-                            />
+                            <i className={`fa-regular fa-heart`} />
                         </OverlayTrigger>
                     )}
                     {likes_count}
