4ea9feaeb3b67320db16ff28e74530d2a17c5bc4
JoeYip13 <>
2023-11-15 10:33:14 +0000

    feat(models): add event model to comment

--- comments/models.py
+++ comments/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from events.models import Event
 
 
 class Comment(models.Model):
     """
-    Comment model, related to User and Post
+    Comment model, related to User and Post and event
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
