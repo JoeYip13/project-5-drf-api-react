58c8928f171efbc742724513bc0ecbaf18db0035
JoeYip13 <>
2023-11-15 16:38:27 +0000

    feat(views): add bookmark list view and detail view

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api_ultimate_m_car.permissions import IsOwnerOrReadOnly
+from bookmarks.models import Bookmark
+from bookmarks.serializers import BookmarkSerializer
 
-# Create your views here.
+
+class BookmarkList(generics.ListCreateAPIView):
+    """
+    List bookmarks or create a bookmark if logged in.
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BookmarkDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a bookmark or delete it by id if you own it.
+    """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
