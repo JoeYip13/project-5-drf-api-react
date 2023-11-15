2eace75fec686d3ae98b1f45afc98402e01e04d9
JoeYip13 <>
2023-11-15 13:20:23 +0000

    feat(pages): add create and edit event pages

---
+++ frontend/src/pages/events/EventCreateForm.js
@@ -0,0 +1,203 @@
+import React, { useRef, useState } from "react";
+
+// Bootstrap components
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+
+import Upload from "../../assets/upload_96x96.png";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
+
+function EventCreateForm() {
+    useRedirect("loggedOut");
+    const [errors, setErrors] = useState({});
+
+    const [eventData, setEventData] = useState({
+        title: "",
+        description: "",
+        event_date: "",
+        location: "",
+        image: "",
+    });
+
+    const {
+        title,
+        description,
+        event_date,
+        image,
+        location,
+    } = eventData;
+
+    const imageInput = useRef(null);
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setEventData({
+            ...eventData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleChangeImage = (event) => {
+        if (event.target.files.length) {
+            URL.revokeObjectURL(image);
+            setEventData({
+                ...eventData,
+                image: URL.createObjectURL(event.target.files[0]),
+            });
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+
+        formData.append("title", title);
+        formData.append("description", description);
+        formData.append("image", imageInput.current.files[0]);
+        formData.append("event_date", event_date);
+        formData.append("location", location);
+        console.log(Object.fromEntries(formData.entries()));
+
+        try {
+            const { data } = await axiosReq.post("/events/", formData);
+            history.push(`/events/${data.id}`);
+        } catch (error) {
+            console.log(error);
+            if (error.response?.status !== 401) {
+                setErrors(error.response?.data);
+            }
+        }
+    };
+
+    const textFields = (
+        <div className="text-center">
+            <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+            <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={6}
+                    name="description"
+                    value={description}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Event Date</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="event_date"
+                    value={event_date}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Location</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="location"
+                    value={location}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+
+            <Button
+                className={`${btnStyles.Button} ${btnStyles.Bright}`}
+                onClick={() => history.goBack()}
+            >
+                Cancel
+            </Button>
+            <Button
+                className={`${btnStyles.Button} ${btnStyles.Bright}`}
+                type="submit"
+            >
+                Create
+            </Button>
+        </div>
+    );
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+                    <Container
+                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+                    >
+                        <Form.Group className="text-center">
+                            {image ? (
+                                <>
+                                    <figure>
+                                        <Image
+                                            className={appStyles.Image}
+                                            src={image}
+                                            rounded
+                                        />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Bright} btn`}
+                                            htmlFor="image-upload"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
+                                <Form.Label
+                                    className="d-flex justify-content-center"
+                                    htmlFor="image-upload"
+                                >
+                                    <Asset
+                                        src={Upload}
+                                        message="Click or tap to upload an image"
+                                    />
+                                </Form.Label>
+                            )}
+
+                            <Form.File
+                                className="d-none"
+                                id="image-upload"
+                                accept="image/*"
+                                onChange={handleChangeImage}
+                                ref={imageInput}
+                            />
+                        </Form.Group>
+                        <div className="d-md-none">{textFields}</div>
+                    </Container>
+                </Col>
+                <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+                    <Container className={appStyles.Content}>
+                        {textFields}
+                    </Container>
+                </Col>
+            </Row>
+        </Form>
+    );
+}
+
+export default EventCreateForm;
