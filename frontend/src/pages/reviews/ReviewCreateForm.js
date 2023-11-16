da68ed8eba0f30b4c6870d02e663b27df4f84a79
JoeYip13 <>
2023-11-16 18:53:01 +0000

    feat(pages): add review create form

---
+++ frontend/src/pages/reviews/ReviewCreateForm.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function ReviewCreateForm(props) {
+    const { event, setEvent, setReviews, profileImage, profile_id } = props;
+    const [review, setReview] = useState("");
+    const [rating, setRating] = useState("");
+
+    const handleReviewChange = (event) => {
+        setReview(event.target.value);
+    };
+
+    const handleRatingChange = (event) => {
+        setRating(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await axiosRes.post("/reviews/", {
+                review,
+                event,
+            });
+            setReviews((prevReviews) => ({
+                ...prevReviews,
+                results: [data, ...prevReviews.results],
+            }));
+            setEvent((prevEvent) => ({
+                results: [
+                    {
+                        ...prevEvent.results[0],
+                        reviews_count: prevEvent.results[0].reviews_count + 1,
+                    },
+                ],
+            }));
+            setReview("");
+            setRating("");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <Form className="mt-2" onSubmit={handleSubmit}>
+            <Row>
+                <Col md="1" lg="1">
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profileImage} />
+                    </Link>
+                </Col>
+                <Col xs="auto" md="4" lg="4">
+                    <Form.Group>
+                        <Form.Label className="d-none">Rating</Form.Label>
+                        <Form.Control
+                            className={styles.Form}
+                            as="select"
+                            name="rating"
+                            value={rating}
+                            onChange={handleRatingChange}
+                        >
+                            <option key="blankChoice" hidden value>
+                                {" "}
+                                Rating{" "}
+                            </option>
+                            <option value="5 stars">5 stars</option>
+                            <option value="4 stars">4 stars</option>
+                            <option value="3 stars">3 stars</option>
+                            <option value="2 stars">2 stars</option>
+                            <option value="1 star">1 star</option>
+                            <option value="0 stars">0 stars</option>
+                        </Form.Control>
+                    </Form.Group>
+                    {errors.rating?.map((message, idx) => (
+                        <Alert key={idx} variant="warning">
+                            {message}
+                        </Alert>
+                    ))}
+                </Col>
+                <Col md="7" lg="7">
+                    <Form.Group>
+                        <Form.Label className="d-none">Review</Form.Label>
+                        <Form.Control
+                            className={styles.Form}
+                            placeholder="My review..."
+                            as="textarea"
+                            value={review}
+                            onChange={handleReviewChange}
+                            rows={1}
+                        />
+                    </Form.Group>
+                    {errors.review?.map((message, idx) => (
+                        <Alert key={idx} variant="warning">
+                            {message}
+                        </Alert>
+                    ))}
+                </Col>
+            </Row>
+            <button
+                className={`${styles.Button} btn d-block ml-auto`}
+                disabled={!review.trim()}
+                type="submit"
+            >
+                post
+            </button>
+            {errors.detail && (
+                <Alert variant="warning" className="mt-3">
+                    {errors.detail}
+                </Alert>
+            )}
+        </Form>
+    );
+}
+
+export default ReviewCreateForm;
