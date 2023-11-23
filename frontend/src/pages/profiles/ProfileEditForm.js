260cacc1c9473434f924cc1ccb44fdae38df8532
JoeYip13 <>
2023-11-23 11:36:08 +0000

    style(color): adjust various button colors and main app color

--- frontend/src/pages/profiles/ProfileEditForm.js
+++ frontend/src/pages/profiles/ProfileEditForm.js
@@ -102,13 +102,13 @@ const ProfileEditForm = () => {
                 </Alert>
             ))}
             <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                className={`${btnStyles.Button} ${btnStyles.Black}`}
                 onClick={() => history.goBack()}
             >
                 cancel
             </Button>
             <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                className={`${btnStyles.Button} ${btnStyles.Black}`}
                 type="submit"
             >
                 save
@@ -134,13 +134,14 @@ const ProfileEditForm = () => {
                             ))}
                             <div>
                                 <Form.Label
-                                    className={`${btnStyles.Button} ${btnStyles.Blue} btn my-auto`}
+                                    className={`${btnStyles.Button} ${btnStyles.Black} btn my-auto`}
                                     htmlFor="image-upload"
                                 >
                                     Change the image
                                 </Form.Label>
                             </div>
                             <Form.File
+                                className="d-none"
                                 id="image-upload"
                                 ref={imageFile}
                                 accept="image/*"
