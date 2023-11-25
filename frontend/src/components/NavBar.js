5662e060fb9974bfd9a8379246123a8816f8804c
JoeYip13 <>
2023-11-25 11:21:09 +0000

    feat(signout): add confirmation prompt to sign out

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,16 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 // Bootstrap Components
+import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Spinner from "react-bootstrap/Spinner";
 
 // Import Logo
 import logo from "../assets/M-logo.png";
 
 // CSS Styles
 import styles from "../styles/NavBar.module.css";
+import btnStyles from "../styles/Button.module.css";
 
 // React router
 import { NavLink } from "react-router-dom";
@@ -34,13 +38,26 @@ const NavBar = () => {
 
     const [toggleNavBar, setToggleNavBar] = useState(false);
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [loading, setLoading] = useState(false);
+
     const handleSignOut = async () => {
+        setLoading(true); // Set loading to true
         try {
             await axios.post("dj-rest-auth/logout/");
             setCurrentUser(null);
             removeTokenTimestamp();
         } catch (err) {
             console.log(err);
+        } finally {
+            // Delay for 3 seconds
+            setTimeout(() => {
+                handleClose(); // Close the modal
+                setLoading(false); // Rest loading state
+            }, 2000);
         }
     };
 
@@ -93,7 +110,7 @@ const NavBar = () => {
                 <i className="fa-regular fa-calendar-days"></i>Events
             </NavLink>
 
-            <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+            <NavLink className={styles.NavLink} to="/" onClick={handleShow}>
                 <i class="fa-solid fa-right-from-bracket"></i>Sign out
             </NavLink>
             <NavLink
@@ -159,40 +176,82 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar
-            expanded={toggleNavBar}
-            className={styles.NavBar}
-            expand="md"
-            fixed="top"
-        >
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand className={styles.logo}>
-                        <span className={styles.logoText}>Ultimate</span>{" "}
-                        <img src={logo} alt="logo" height="45" />{" "}
-                        <span className={styles.logoText}>Car</span>
-                    </Navbar.Brand>
-                </NavLink>
-                {currentUser && addIcon}
-                <Navbar.Toggle
-                    onClick={() => setToggleNavBar(!toggleNavBar)}
-                    aria-controls="basic-navbar-nav"
-                />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            exact
-                            to="/"
+        <>
+            <Navbar
+                expanded={toggleNavBar}
+                className={styles.NavBar}
+                expand="md"
+                fixed="top"
+            >
+                <Container>
+                    <NavLink to="/">
+                        <Navbar.Brand className={styles.logo}>
+                            <span className={styles.logoText}>Ultimate</span>{" "}
+                            <img src={logo} alt="logo" height="45" />{" "}
+                            <span className={styles.logoText}>Car</span>
+                        </Navbar.Brand>
+                    </NavLink>
+                    {currentUser && addIcon}
+                    <Navbar.Toggle
+                        onClick={() => setToggleNavBar(!toggleNavBar)}
+                        aria-controls="basic-navbar-nav"
+                    />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <NavLink
+                                className={styles.NavLink}
+                                activeClassName={styles.Active}
+                                exact
+                                to="/"
+                            >
+                                <i class="fa-solid fa-bars-staggered"></i>Feed
+                            </NavLink>
+                            {currentUser ? loggedInIcons : loggedOutIcons}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Sign Out</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {loading ? (
+                        <p>Signing out...</p>
+                    ) : (
+                        <p>Are you sure you want to sign out?</p>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    {!loading && (
+                        <>
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Black}`}
+                                onClick={handleClose}
+                            >
+                                No
+                            </Button>
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Black}`}
+                                onClick={handleSignOut}
+                            >
+                                Yes
+                            </Button>
+                        </>
+                    )}
+                    {loading && (
+                        <Spinner
+                            animation="border"
+                            variant="light"
+                            role="status"
                         >
-                            <i class="fa-solid fa-bars-staggered"></i>Feed
-                        </NavLink>
-                        {currentUser ? loggedInIcons : loggedOutIcons}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                            <span className="sr-only">Signing out...</span>
+                        </Spinner>
+                    )}
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
 
